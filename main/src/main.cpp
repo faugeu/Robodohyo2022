8f7569744c13ee89375fa7ed931e687fdd3c5e65
Tin <10422078@student.vgu.edu.vn>
2022-12-29 18:31:27 +0700

    Hello

--- main/src/main.cpp
+++ main/src/main.cpp
@@ -1,5 +1,6 @@
 #include <Arduino.h>
 #include <SharpIR.h> 
+#include <NewPing.h>
 
 // Button
 const int buttonLeft = 11;
@@ -24,10 +25,14 @@ const int ultraLeftTrig = 2;
 const int ultraLeftEcho = A3;
 const int ultraRightTrig = 13;
 const int ultraRightEcho = A5;
+const int maxDistance = 200;
 SharpIR sensor(SharpIR::GP2Y0A02YK0F, A4);
 
 // sensor
 int colourThreshold;
+NewPing sonarLeft(ultraLeftTrig, ultraLeftEcho, maxDistance);
+NewPing sonarRight(ultraRightTrig, ultraRightEcho, maxDistance);
+
 
 // motor
 int ultrasPin[2][2] = {{ultraRightTrig, ultraRightEcho}, {ultraLeftTrig, ultraLeftEcho}};
@@ -36,160 +41,19 @@ double reverseConst = 1;
 
 int count = 0;
 
-int getValueIR(){
-  int distance = sensor.getDistance(); //get distance from IR
-  return distance;
-}
-
-
-int getValueUltra(int side) {
-  // -1 is left, 1 is right
-  long duration, distance;
-  int trig, echo;
-  if (side == -1) {
-    trig = ultraLeftTrig;
-    echo = ultraLeftEcho;
-  }
-  else {
-    trig = ultraRightTrig;
-    echo = ultraRightEcho;
-  }
-  digitalWrite(trig, LOW);  // Added this line
-  delayMicroseconds(2); // Added this line
-  digitalWrite(trig, HIGH);
-  delayMicroseconds(10); // Added this line
-  digitalWrite(trig, LOW);
-  duration = pulseIn(echo, HIGH);
-  distance = (duration/2) / 29.1;
-  if (distance > 150) {
-    distance = 0;
-  }
-  return distance;
-}
-
-
-void calibrate() {
-  while (millis() < 500) {
-    int lineLeftValue = analogRead(lineLeft);
-    Serial.println(lineLeftValue);
-    int lineMidValue = analogRead(lineMid);
-    Serial.println(lineMidValue);
-    int lineRightValue = analogRead(lineRight);
-    Serial.println(lineRightValue);
-    colourThreshold = (lineLeftValue + lineMidValue + lineRightValue) / 12;
-  }
-}
-
-
-//startRoutine Left
-
-
-
-void runStraight(int left, int right) {
-  Serial.println("Run forward");
-  digitalWrite(in1, LOW);
-  digitalWrite(in2, HIGH);
-  digitalWrite(in3, HIGH);
-  digitalWrite(in4, LOW);
-  analogWrite(enRight, left); 
-  analogWrite(enLeft, right);
-}
-
-
-void runReverse(int left, int right) {
-  // -1 is left, 0 is mid and 1 is right
-  digitalWrite(in1, HIGH);
-  digitalWrite(in2, LOW);
-  digitalWrite(in3, LOW);
-  digitalWrite(in4, HIGH);
-  analogWrite(enRight, right); 
-  analogWrite(enLeft, left);
-}
-
-
-void accelerate(int direction, int maxSpeed) {
-  // -1 is reverse, 1 is straight
-  int delayTime = 20; // milliseconds between each speed step
-  // accelerate the motor
-  if (direction == 1) {
-    for(int speed = 0; speed <= maxSpeed; speed++) { // counts from 0 to 255 (max speed) using the variable "speed"
-      // runStraight(speed); // set the new speed
-      delay(delayTime); // delay between speed steps
-    }
-  }
-  else {
-    for(int speed = 0; speed <= maxSpeed; speed++) { // counts from 0 to 255 (max speed) using the variable "speed"
-      runReverse(0, speed); // set the new speed
-      delay(delayTime); // delay between speed steps
-    }
-  }
-  
-}
-
-
-void attack() {
-  int valueIR = getValueIR();
-  int valueUltraLeft = getValueUltra(1);
-  int valueUltraright = getValueUltra(0);
-  //Move robot forward opponent
-  
-  if (valueIR <= 80){ //range for robot to attack
-  }
-  else{//out of attack range
-    //run or continue to detect
-  } 
-}
-
-
-void backOff() {
-  Serial.println("Back off");
-  runStraight(0,0);
-  
-  runReverse(150, 170);
-  delay(300);
-
-  runReverse(255,0);
-  delay(900);
-
-}
-
-void startRoutineLeft() {
-  Serial.println("Start routine Left");
-  delay(3000);
-
-  //Spin left
-  runStraight(0, 100);
-  delay(100);
-
-  //Run straight
-  runStraight(70, 58);
-  delay(400);
-
-  //Turn and scan with millis
-
-  // Calibrate the QTI
-  // calibrate();
-  // count++;
-}
-
-void startRoutineRight() {
-  Serial.println("Start routine Right");
-  delay(3000);
-
-  //Spin left
-  runStraight(100, 0);
-  delay(1000);
-
-  //Run straight
-  runStraight(70, 58);
-  delay(400);
-
-  //Turn and scan with millis
-
-  // Calibrate the QTI
-  // calibrate();
-  // count++;
-}
+int getValueIR();
+int getValueUltraLeft();
+int getValueUltraRight();
+void calibrate();
+void runStraight(int left, int right);
+void runLeft(int left, int right);
+void runRight(int left, int right);
+void runReverse(int left, int right);
+void accelerate(int direction, int maxSpeed);
+void attack();
+void backOff();
+void startRoutineLeft();
+void startRoutineRight();
 
 void setup() {
   Serial.begin(9600);   
@@ -213,92 +77,249 @@ void setup() {
 
 
 void loop() {
-  if (count == 0){
-    int button_status = digitalRead(buttonLeft);   
-  Serial.println(button_status);              
-  delay(100);                                
-  if (button_status == 1){
-    startRoutineLeft();
-  }
-  } 
-  
   // if (count == 0){
-  //   int signalLeft = digitalRead(buttonLeft);
-  //   Serial.println(signalLeft);
+  //   int buttonStatusLeft = digitalRead(buttonLeft);
+  //   int buttonStatusRight = digitalRead(buttonRight);   
+  //   Serial.println(buttonStatusLeft);
+  //   Serial.println(buttonStatusRight);
+  //   delay(100);                       
 
-  //   int signalRight = digitalRead(buttonRight);
-  //   Serial.println(signalRight);
-  //   delay(200);
-
-  //   if (signalLeft == 1){
+  //   if (buttonStatusLeft == 1){
   //     startRoutineLeft();
   //     count++;
   //   }
-  //   else if (signalRight == 1) {
+  //   else if (buttonStatusRight == 1){
   //     startRoutineRight();
   //     count++;
   //   }
-  //   else{
-  //       int lineLeftValue = analogRead(lineLeft);
-  //   Serial.print("Left:");
-  //   Serial.println(lineLeftValue);
-
-  //   //Mid sensor white = 21
-  //   int lineMidValue = analogRead(lineMid);
-  //   Serial.print("Mid: ");
-  //   Serial.println(lineMidValue);
-
-  //   //Right sensor white = 26
-  //   int lineRightValue = analogRead(lineRight);
-  //   Serial.print("Right: ");
-  //   Serial.println(lineRightValue);
-
-  //   if (lineLeftValue < 30){
-  //     backOff();
-  //   }
-  //   else if (lineMidValue < 30){
-  //     backOff();
-  //   }
-  //   else if (lineRightValue < 30){
-  //     backOff();
-  //   }
-  //   else{
-  //     runStraight(150,170);
-  //   }
-  //     }
   // }
-    // startRoutine();
-  //Left sensor white = 25
-  //Left sensor black = 
+  // else{
+    int lineLeftValue = analogRead(lineLeft);
+    Serial.print("Left:");
+    Serial.println(lineLeftValue);
+
+    //Mid sensor white = 21
+    int lineMidValue = analogRead(lineMid);
+    Serial.print("Mid: ");
+    Serial.println(lineMidValue);
+
+    //Right sensor white = 26
+    int lineRightValue = analogRead(lineRight);
+    Serial.print("Right: ");
+    Serial.println(lineRightValue);
+
+    if (lineLeftValue < 30){
+      backOff();
+    }
+    else if (lineMidValue < 30){
+      backOff();
+    }
+    else if (lineRightValue < 30){
+      backOff();
+    }
+    else{
+      runStraight(255,255);
+    }
+}
+
+int getValueIR()
+{
+  int distance = sensor.getDistance(); //get distance from IR
+  return distance;
+}
+
+
+int getValueUltraLeft() 
+{
+  // -1 is left, 1 is right
+  int distance = sonarLeft.ping_cm();
+  if (distance > 150) {
+    distance = 0;
+  }
+  return distance;
+}
+
+
+int getValueUltraRight() 
+{
+  // -1 is left, 1 is right
+  int distance = sonarRight.ping_cm();
+  if (distance > 150) {
+    distance = 0;
+  }
+  return distance;
+}
+
+
+void calibrate() 
+{
+  while (millis() < 500) {
+    int lineLeftValue = analogRead(lineLeft);
+    Serial.println(lineLeftValue);
+    int lineMidValue = analogRead(lineMid);
+    Serial.println(lineMidValue);
+    int lineRightValue = analogRead(lineRight);
+    Serial.println(lineRightValue);
+    colourThreshold = (lineLeftValue + lineMidValue + lineRightValue) / 12;
+  }
+}
+
+
+//startRoutine Left
+
+
+
+void runStraight(int left, int right) 
+{
+  Serial.println("Run forward");
+  digitalWrite(in1, LOW);
+  digitalWrite(in2, HIGH);
+  digitalWrite(in3, HIGH);
+  digitalWrite(in4, LOW);
+  analogWrite(enRight, right); 
+  analogWrite(enLeft, left);
+}
+
+void runLeft(int left, int right) 
+{
+  Serial.println("Run forward");
+  digitalWrite(in1, HIGH);
+  digitalWrite(in2, LOW);
+  digitalWrite(in3, HIGH);
+  digitalWrite(in4, LOW);
+  analogWrite(enRight, right); 
+  analogWrite(enLeft, left);
+}
+
+void runRight(int left, int right) 
+{
+  Serial.println("Run forward");
+  digitalWrite(in1, LOW);
+  digitalWrite(in2, HIGH);
+  digitalWrite(in3, LOW);
+  digitalWrite(in4, HIGH);
+  analogWrite(enRight, right); 
+  analogWrite(enLeft, left);
+}
+
+void runReverse(int left, int right) 
+{
+  // -1 is left, 0 is mid and 1 is right
+  digitalWrite(in1, HIGH);
+  digitalWrite(in2, LOW);
+  digitalWrite(in3, LOW);
+  digitalWrite(in4, HIGH);
+  analogWrite(enRight, right); 
+  analogWrite(enLeft, left);
+}
+
+
+void accelerate(int direction, int maxSpeed) 
+{
+  // -1 is reverse, 1 is straight
+  int delayTime = 20; // milliseconds between each speed step
+  // accelerate the motor
+  if (direction == 1) {
+    for(int speed = 0; speed <= maxSpeed; speed++) { // counts from 0 to 255 (max speed) using the variable "speed"
+      runStraight(speed,0); // set the new speed
+      delay(delayTime); // delay between speed steps
+    }
+  }
+  else {
+    for(int speed = 0; speed <= maxSpeed; speed++) { // counts from 0 to 255 (max speed) using the variable "speed"
+      runReverse(0, speed); // set the new speed
+      delay(delayTime); // delay between speed steps
+    }
+  }
   
+}
 
 
-  // put your main code here, to run repeatedly:
-  // while ((digitalRead(buttonLeft) || digitalRead(buttonRight)) && count == 0) {
-  //   startRoutine();
-
-  // }
+void attack() 
+{
+  int valueIR = getValueIR();
+  //Move robot forward opponent
   
-  // while (!digitalRead(buttonLeft) && !digitalRead(buttonRight && count > 0)) {
-  //   // Edge is detected on the right.
-  //   if (analogRead(lineRight) < colourThreshold) {
-  //     // Back off and make a U-Turn to the left.
-  //     backOff(-1);
+  if (valueIR <= 80){ //range for robot to attack
+  }
+  else{//out of attack range
+    //run or continue to detect
+  } 
+}
+
+
+void backOff() 
+{
+  Serial.println("Back off");
+  runStraight(0,0);
+  
+  runReverse(150, 170);
+  delay(200);
+
+  runReverse(255,0);
+  delay(900);
+
+}
+
+void startRoutineLeft() 
+{
+  Serial.println("Start routine Left");
+  delay(3000);
+
+  //Spin left
+  Serial.println("Spin Left");
+  int timeStart =millis();
+  Serial.println(getValueIR());
+  while(getValueIR()){
+    Serial.println(getValueIR());
+    Serial.println("Spin");
+    runStraight(0,100);
+  }
+  delay(5000);
+  //Run straight
+  // Serial.println("Run straight");
+  // runStraight(70, 58);
+  // delay(2000);
+
+  //Turn and scan with millis
+  // int timeStart = millis();
+  // int timeEnd = 0;
+  // while (timeEnd - timeStart <= 500){
+  //   runStraight(0, 100);
+  //   //Start scanning
+  //   int disUltraLeft = getValueUltra(-1);
+  //   int disUltraRight = getValueUltra(1);
+  //   int disIR = getValueIR();
+  //   //detect
+  //   if (disIR > 0 && disIR < 80){
+  //     runStraight(70, 58); // Run straight
   //   }
-  //   else if (analogRead(lineMid) < colourThreshold) {      
-  //     // Go backward and turn back.
-  //     backOff(0);
-  //   }
-  //   // Edge is detected on the left.
-  //   else if (analogRead(lineLeft) < colourThreshold) {      
-  //     // Back off and make a U-Turn to the right.
-  //     backOff(1);
-  //   }
-  //   else {
-  //     runStraight(58);
-  //   }
-  //   while (digitalRead(buttonLeft) || digitalRead(buttonRight)){
-  //     while(1);
-  //   }
+  //   timeEnd = millis();
   // }
+
+  // Calibrate the QTI
+  // calibrate();
+  // count++;
+}
+
+void startRoutineRight() 
+{
+  Serial.println("Start routine Right");
+  delay(3000);
+
+  //Spin Right
+  Serial.println("Turn right");
+  runStraight(100, 0);
+  delay(1000);
+
+  //Run straight
+  Serial.println("Run straight");
+  runStraight(70, 58);
+  delay(2000);
+
+  //Turn and scan with millis
+
+  // Calibrate the QTI
+  // calibrate();
+  // count++;
 }
